1520dfd3e196b73addc245dc00056a962f47bf55
Pranav Joshi <pranavjoshi2893@gmail.com>
2026-09-25 18:01:17 +0530

    readme updated

--- cmd/root.go
+++ cmd/root.go
@@ -18,7 +18,7 @@ consistent project structures and architectural boundaries.
 It helps you establish boundaries between components,
 enforce architectural rules, and keep your codebase aligned
 with the structure you define.`,
-	Version: "0.1.4-nightly.1",
+	Version: "0.1.4-nightly.2",
 }
 
 func Execute() {
